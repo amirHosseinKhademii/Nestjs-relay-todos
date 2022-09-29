2ccdb93e476cc24e6c18b83313369077971c8f5b
amirHosseinKhademii <sourena2575@gmail.com>
2022-09-29 10:13:27 +0200

    Add global pipe

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
 }
