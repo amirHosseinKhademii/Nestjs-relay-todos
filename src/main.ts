4b7405f7c5501dbf8ca2735cd4b19d9c46ead277
amirHosseinKhademii <sourena2575@gmail.com>
2022-09-30 20:50:20 +0200

    Add user interceptor to transform

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3001);
 }
 bootstrap();
